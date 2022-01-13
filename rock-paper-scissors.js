2cea251fea754d43eee374a0eb240ad2f95f557c
abuabu <alhajimsabu@gmail.com>
2022-01-13 23:40:52 +0000

    rock-paper-scissors.js game functions added

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,92 +1,156 @@
-// capitalize first letter of player selection string 
+const roundField = document.querySelector('.roundField');
+const roundFieldSubmit = document.querySelector('.roundFieldSubmit');
+const selections = document.querySelector('.selection');
+const winOrLoss = document.querySelector('.winOrLoss');
+const finalResult = document.querySelector('.finalResult');
+const resetbtn = document.querySelector('.resetbtn');
+    
+let playCount = 0;
+let resetButton;
+let p = 0; // player score
+let c = 0; // computer score
+let t = 0; // draw
+
 function playerSelectionCapitalized(str){
     const capitalized = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
     return capitalized;
 }
 
-const playerSelection = 'Rock';
-const computerSelection = computerPlay();
-
-// create random selection by computer
 function computerPlay(){
     const array = ['Rock', 'Paper', 'Scissors'];
     const random = Math.floor(Math.random() * array.length);
     return array[random];
 }
 
-// create single playing round
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+
 function playRound (playerSelection, computerSelection){
 
-    playerSelection = playerSelectionCapitalized(prompt('Player Selection'));
+    playerSelection = playerSelectionCapitalized(roundField.value);
     computerSelection = computerPlay();
 
-    // compare both players selections and return a result 
     if (playerSelection === 'Rock' && computerSelection === 'Rock'){
-        console.log(`RR - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`); 
-        return 'It\'s a tie';       
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'It\'s a tie';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper'){
-        console.log(`RP - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`);        
-        return 'Computer wins';
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'Computer wins';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
-        console.log(`RS - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`); 
-        return 'Player wins';        
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'Player wins';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
-        console.log(`PR - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`); 
-        return 'Player wins';        
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'Player wins';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Paper' && computerSelection === 'Paper'){
-        console.log(`PP - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`);
-        return 'It\'s a tie';         
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'It\'s a tie';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
-        console.log(`PS - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`);
-        return 'Computer wins';        
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'Computer wins';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
-        console.log(`SR - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`);
-        return 'Computer wins';        
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'Computer wins';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
-        console.log(`SP - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`);       
-        return 'Player wins'; 
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'Player wins';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors'){
-        console.log(`SS - playerSelection: ${playerSelection} computerSelection: ${computerSelection}`); 
-        return 'It\'s a tie';        
+        selections.textContent += ` ${playerSelection}-${computerSelection} `; 
+        winOrLoss.textContent = 'It\'s a tie';
+        finalResult.textContent = ' ';
+        return winOrLoss.textContent;
     } 
 }
 
-// start and repeat to desired play round
+function setGameOver() {
+    roundField.disabled = true;
+    roundFieldSubmit.disabled = true;
+
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start New Game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    playCount = 0;
+
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (const resetPara of resetParas) {
+        resetPara.textContent = '';
+    }
+        resetButton.parentNode.removeChild(resetButton);
+
+        roundField.disabled = false;
+        roundFieldSubmit.disabled = false;
+        roundField.value = '';
+        roundField.focus();
+        finalResult.style.backgroundColor = '#1F2937';
+    }
+
 function game() {
-    let i = 1;
-    let p = 0; // player score
-    let c = 0; // computer score
-    let t = 0; // draw
-
-    do {
-        // set result of round
-        let res = playRound(); 
-
-        switch (res) {
-            // increment player score in case player wins the round
+    const roundFieldCheck = roundField.value;
+    if (roundFieldCheck === ' ' || roundFieldCheck === '') {
+        alert ('Valid selection needed');
+        roundField.value = '';
+        roundField.focus();
+    } else {
+        let result = playRound(); 
+        switch (result) {
             case 'Player wins': 
                 p++; 
                 break;
-
-            // increment computer score in case player wins the round
             case 'Computer wins': 
                 c++; 
                 break;
-            // increment draw in case the round is a tie
             case 'It\'s a tie':
                 t++;
                 break;
         } 
-        i++;
-    } while (i <= 5);
-        console.log('END OF GAME');
-
-// compare scores from game and determine winner
-    if (p > c) {
-        return 'Player wins the game!';
-    } else if (p < c) {
-        return 'Computer wins the game!';
-    } else if (p === c) {
-        return 'It\' a tie!';
+        playCount++;
+        roundField.value = '';
+        roundField.focus();
+        finalResult.textContent = `Number of rounds left: ${5-playCount}`
+        finalResult.style.color = 'red';
+        end();
+    }
+}
+// compare scores from game and determine final result
+function end(){
+    if (playCount === 5 && p > c) {
+        finalResult.textContent = 'Congratulations! You won!';
+        finalResult.style.backgroundColor = 'green';
+        finalResult.style.color = 'white';
+        winOrLoss.textContent = 'END OF GAME';
+        setGameOver();
+    } else if (playCount === 5 && p < c) {
+        finalResult.textContent = 'Sorry! Computer won!';
+        finalResult.style.backgroundColor = 'blue';
+        finalResult.style.color = 'white';
+        winOrLoss.textContent = 'END OF GAME';
+        setGameOver();
+    } else if (playCount === 5 && p === c) {
+        finalResult.style.backgroundColor = 'Yellow';
+        finalResult.style.color = '#1F2937';
+        finalResult.textContent = 'IT\'S A TIE! Please play again!!';
+        winOrLoss.textContent = 'END OF GAME';
+        setGameOver();
     } 
 }
+roundFieldSubmit.addEventListener('click', game);
